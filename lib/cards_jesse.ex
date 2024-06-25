afb79c0d81593433fa176ea98650c3aab8ce7ee2
roostercrab <jesseshea498@gmail.com>
2024-06-25 08:05:10 -0700

    init commit

---
+++ lib/cards_jesse.ex
@@ -0,0 +1,18 @@
+defmodule Deck do
+
+  def create_deck do
+    list_of_values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    list_of_suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    for suit <- list_of_suits, value <- list_of_values, do: "#{value} of #{suit}"
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def deal(deck, hand_size)do
+    Enum.split(deck, hand_size)
+  end
+end
+
+#IO.inspect(deck)
---
+++ lib/notes.ex
@@ -0,0 +1,15 @@
+defmodule Notes do
+  @moduledoc """
+  To create docmodule you have to put:
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.12"}
+    ]
+  end
+
+  in mix.exs, then you have to install it with mix deps.get
+  To generate the html file you have to run mix docs
+
+  """
+end
