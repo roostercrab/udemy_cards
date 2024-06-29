f6673b053c92d08b73cbae8206a66a8172238653
roostercrab <jesseshea498@gmail.com>
2024-06-29 12:12:08 -0700

    second commit

--- lib/cards_jesse.ex
+++ lib/cards_jesse.ex
@@ -1,7 +1,21 @@
 defmodule Deck do
-
   def create_deck do
-    list_of_values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    list_of_values = [
+      "Ace",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
     list_of_suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
     for suit <- list_of_suits, value <- list_of_values, do: "#{value} of #{suit}"
   end
@@ -10,9 +24,9 @@ defmodule Deck do
     Enum.shuffle(deck)
   end
 
-  def deal(deck, hand_size)do
+  def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 end
 
-#IO.inspect(deck)
+# IO.inspect(deck)
